d4b18f9a2bf3041d8bf8d6ebe3777e762a05a6a8
shbert <smarthome@rhenjes.de>
2024-02-11 11:06:22 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -7,16 +7,67 @@ terraform {
     }
 }
 
+variable "runpodApiEndpoint" {}
+variable "runpodApiKey" {}
+
+variable "name" {}
+variable "cloudType" {}
+variable "ports" {}
+variable "gpuTypeId" {}
+variable "gpuCount" {}
+variable "imageName" {}
+variable "containerDiskInGb" {}
+variable "volumeInGb" {}
+variable "volumePath" {}
+
 provider "graphql" {
-    url = ""
-    headers = {
-
-    }
-
+    url = "${var.runpodApiEndpoint}?api_key=${var.runpodApiKey}"
 }
 
 
-data "graphql_query" "basic_query" {
-    query_variables = {}
-    query = file("./gpu.gql")
+#data "graphql_query" "basic_query" {
+#    query_variables = {}
+#    query = file("./queries/gpuTypes.graphql")
+#}
+
+#locals {
+#    gpu_data = jsondecode(data.graphql_query.basic_query.query_response)
+#    all_gpuIds = [for l_gpuType in local.gpu_data.data.gpuTypes : l_gpuType.id]
+#}
+
+#output "graphql_query_out" {
+#    value = local.all_gpuIds
+#}
+
+resource "graphql_mutation" "basic_mutation" {
+    mutation_variables = {
+        #"cloudType" = "${var.cloudType}"
+        "name" = "${var.name}"
+        "ports" = "${var.ports}"
+        "gpuTypeId" = "${var.gpuTypeId}"
+        "gpuCount" = var.gpuCount
+        "imageName" = "${var.imageName}"
+        "containerDiskInGb" = var.containerDiskInGb
+        "volumeInGb" = var.volumeInGb
+        #"volumePath" = "${var.volumePath}"
+    }
+
+    # read_query_variables = {
+    #   "podId" = "afdsfsadfasdf"
+    # }
+
+    # delete_mutation_variables = {
+    #   "podId" = "asdasd"
+    # }
+
+    compute_mutation_keys = {
+        "podId" = "data.podFindAndDeployOnDemand.id"
+    }
+
+    compute_from_create = true
+
+    create_mutation = file("./queries/createMutation.qraphql")
+    update_mutation = file("./queries/updateMutation.graphql")
+    delete_mutation = file("./queries/deleteMutation.graphql")
+    read_query      = file("./queries/readQuery.graphql")
 }
