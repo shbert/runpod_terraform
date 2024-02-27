cf890af1aec5c6c9084a1a5be400ad040a8a746f
shbert <dev@moba-connect.de>
2024-02-27 20:11:59 +0100

    formatting

--- main.tf
+++ main.tf
@@ -1,41 +1,39 @@
 terraform {
-    required_providers {
-        graphql = {
-            source = "sullivtr/graphql"
-            version = ">=2.5.4"
-        }
+  required_providers {
+    graphql = {
+      source  = "sullivtr/graphql"
+      version = ">=2.5.4"
     }
+  }
 }
 
-
-
 provider "graphql" {
-    url = "${var.runpodApiEndpoint}?api_key=${var.runpodApiKey}"
+  url = "${var.runpodApiEndpoint}?api_key=${var.runpodApiKey}"
 }
 
 resource "graphql_mutation" "runpod_create_mutation" {
-    mutation_variables = {
-        #"cloudType" = var.cloudType
-        "name" = "${var.name}"
-        "ports" = "${var.ports}"
-        "gpuTypeId" = "${var.gpuTypeId}"
-        "gpuCount" = var.gpuCount
-        "imageName" = "${var.imageName}"
-        "containerDiskInGb" = var.containerDiskInGb
-        "volumeInGb" = var.volumeInGb
-        "dockerArgs" = var.dockerArgs
-        "volumeMountPath" = "${var.volumeMountPath}"
-    }
+  mutation_variables = {
+    #"cloudType" = var.cloudType
+    "name"              = "${var.name}"
+    "ports"             = "${var.ports}"
+    "gpuTypeId"         = "${var.gpuTypeId}"
+    "gpuCount"          = var.gpuCount
+    "imageName"         = "${var.imageName}"
+    "containerDiskInGb" = var.containerDiskInGb
+    "volumeInGb"        = var.volumeInGb
+    "dockerArgs"        = var.dockerArgs
+    "volumeMountPath"   = "${var.volumeMountPath}"
+  }
 
-    compute_mutation_keys = {
-        "podId" = "data.podFindAndDeployOnDemand.id"
-    }
+  compute_mutation_keys = {
+    "podId" = "data.podFindAndDeployOnDemand.id"
+  }
 
-    compute_from_create = true
+  compute_from_create = true
 
-    create_mutation = file("./queries/createMutation.graphql")
-    update_mutation = file("./queries/updateMutation.graphql")
-    delete_mutation = file("./queries/deleteMutation.graphql")
-    read_query      = file("./queries/readQuery.graphql")
+  create_mutation = file("./queries/createMutation.graphql")
+  update_mutation = file("./queries/updateMutation.graphql")
+  delete_mutation = file("./queries/deleteMutation.graphql")
+  read_query      = file("./queries/readQuery.graphql")
 }
 
